d419962b37a440bf85aa8e53d61467c37d53b969
thecederick <thecederick@gmail.com>
2015-12-16 13:03:33 -0700

    first version

---
+++ src/rabbit_exchange_type_header_to_message.erl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author Evgeniy.Paziy
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 16. Dec 2015 11:45 AM
+%%%-------------------------------------------------------------------
+-module(rabbit_exchange_type_header_to_message).
+-author("Evgeniy.Paziy").
+
+%% API
+-export([]).
