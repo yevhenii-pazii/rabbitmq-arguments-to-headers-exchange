60050971317630fa82e722225a8c264318d4d1d7
thecederick <thecederick@gmail.com>
2015-12-16 14:34:41 -0700

    successful installed to RabbitMQ 3.5.6

---
+++ Makefile
@@ -0,0 +1,28 @@
+PACKAGE=rabbitmq_header_to_message_exchange
+RABBIT_VERSION=3.5.6
+ARCH=$(PACKAGE)-$(RABBIT_VERSION)
+DIST_DIR=ez
+EBIN_DIR=ebin
+INCLUDE_DIRS=include
+DEPS_DIR=deps
+DEPS ?=
+DEPS_EZ=$(foreach DEP, $(DEPS), $(DEPS_DIR)/$(DEP).ez)
+RABBITMQ_HOME ?= .
+
+all:
+
+clean:
+	rm -rf $(DIST_DIR)
+	rm -rf $(EBIN_DIR)
+
+distclean:
+	clean
+	rm -rf $(DEPS_DIR)
+
+package:
+	rm -f $(DIST_DIR)/$(ARCH).ez
+	mkdir -p $(DIST_DIR)/$(ARCH)
+	cp -r $(EBIN_DIR) $(DIST_DIR)/$(ARCH)
+	$(foreach EXTRA_DIR, $(INCLUDE_DIRS), cp -r $(EXTRA_DIR) $(DIST_DIR)/$(ARCH);)
+	(cd $(DIST_DIR); zip -r $(ARCH).ez $(ARCH))
+
--- src/rabbit_exchange_type_header_to_message.erl
+++ src/rabbit_exchange_type_header_to_message.erl
@@ -1,13 +1,58 @@
 %%%-------------------------------------------------------------------
-%%% @author Evgeniy.Paziy
-%%% @copyright (C) 2015, <COMPANY>
+%%% @author Evgeniy.Paziy <epaziy@softserveinc.com>
+%%% @copyright (C) 2015, SoftServe inc.
 %%% @doc
 %%%
 %%% @end
 %%% Created : 16. Dec 2015 11:45 AM
 %%%-------------------------------------------------------------------
 -module(rabbit_exchange_type_header_to_message).
--author("Evgeniy.Paziy").
+-author("Evgeniy.Paziy <epaziy@softserveinc.com>").
+
+-behaviour(rabbit_exchange_type).
 
 %% API
--export([]).
+-export([
+  description/0,
+  serialise_events/0,
+  route/2,
+  validate/1,
+  validate_binding/2,
+  create/2,
+  delete/3,
+  policy_changed/2,
+  add_binding/3,
+  remove_bindings/3,
+  assert_args_equivalence/2
+]).
+
+-rabbit_boot_step({?MODULE,
+  [{description, "exchange type argument-to-header"},
+    {mfa,         {rabbit_registry, register,
+      [exchange, <<"argument-to-header">>, ?MODULE]}},
+    {requires,    rabbit_registry},
+    {enables,     kernel_ready}]}).
+
+
+description() ->
+  [{name, <<"argument-to-header">>},
+   {description, <<"Adds exchange argumets to message header">>}].
+
+
+serialise_events() -> false.
+
+route(_,_) ->
+  rabbit_router:match_routing_key('_', ['_']).
+
+
+validate_binding(_X, _B) -> ok.
+
+
+validate(_X) -> ok.
+create(_Tx, _X) -> ok.
+delete(_Tx, _X, _Bs) -> ok.
+policy_changed(_X1, _X2) -> ok.
+add_binding(_Tx, _X, _B) -> ok.
+remove_bindings(_Tx, _X, _Bs) -> ok.
+assert_args_equivalence(X, Args) ->
+  rabbit_exchange:assert_args_equivalence(X, Args).
